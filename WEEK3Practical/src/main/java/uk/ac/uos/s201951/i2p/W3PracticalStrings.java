c8914a1e2eb7e0358e6d3078faef0b9c21397f95
Edward Glendinning <edward.glendinning@bt.com>
2020-10-15 11:25:51 +0100

    Week 3 Practical String tasks

---
+++ WEEK3Practical/src/main/java/uk/ac/uos/s201951/i2p/W3PracticalStrings.java
@@ -0,0 +1,20 @@
+package uk.ac.uos.s201951.i2p;
+
+public class W3PracticalStrings {
+
+	public static void main(String[] args) {
+
+		// String manipulation tasks
+		
+		String name = "Edward";
+		String city = "Ipswich";
+		
+		String nameCity = "My name is " + name + " and I live in " + city + ".";
+		
+		System.out.println(nameCity);
+		
+		
+
+	}
+
+}
